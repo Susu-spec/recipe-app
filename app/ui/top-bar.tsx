decebbbce6459359651f7fd02af073336a09b43e
Susu-spec <suleimansuwaibat@gmail.com>
2024-04-24 11:35:37 +0100

    Update: Commented out link

--- app/ui/top-bar.tsx
+++ app/ui/top-bar.tsx
@@ -4,9 +4,9 @@ import NavLinks from '@/app/ui/nav-links'
 export default function TopNav() {
     return (
         <div className='w-100'>
-            <div>
+            {/* <div>
                 <Link href="/login"></Link>
-            </div>
+            </div> */}
             <div className="flex grow flex-row justify-between space-x-2 items-center md:space-x-0 md:space-y-2">
                 <NavLinks />
                 {/* <div className='hidden h-auto w-full grow rounded-md bg-gray-50 md:block'></div> */}
